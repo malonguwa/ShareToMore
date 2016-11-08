154419c254f6f4f696c8e68fee1b117e3da96115
LONG MA <longma@LONGs-MacBook-Pro.local>
2016-11-08 15:13:44 +0800

    init project

---
+++ ShareToMore/ShareToMore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareToMore
+//
+//  Created by LONG MA on 8/11/16.
+//  Copyright © 2016 HnA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
